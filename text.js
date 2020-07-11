cff2187ccb9676a5f00f21834f170739038326f5
Artur <vitamin.promo@gmail.com>
2020-07-11 16:01:29 +0300

    Добавил txt файл

--- text.js
+++ text.js
@@ -1,7 +1,15 @@
-switcher = 1;
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let quest1 = prompt("Введите обязательную статью расходов в этом месяце");
+let quest2 = prompt("Во сколько обойдется?");
 
-if (switcher) {
-    console.log('asdfsd');
-}
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {quest1 : quest2},
+  optionalExpenses: "",
+  income: "",
+  savings: false,
+};
 
-alert('Hello')
+alert(money / 30);
